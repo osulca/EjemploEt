763ffc97bc6f694f16705e02e24ea8352baeaa39
Omar Sulca <omarsulca@gmail.com>
2022-04-30 16:43:14 -0500

    Metodo 1

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -2,81 +2,29 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    TextView tvMensaje;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("MYAPP", "en Creación");
-        Toast toast = Toast.makeText(this,"en Creación",Toast.LENGTH_LONG);
-        toast.show();
-        tvMensaje = findViewById(R.id.tvMensaje);
-        tvMensaje.setText("en Creación\n");
+        Button boton = findViewById(R.id.btnIntent);
+        boton.setOnClickListener(this);
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("MYAPP", "en Inicio");
-        Toast toast = Toast.makeText(this,"en Inicio",Toast.LENGTH_LONG);
-        toast.show();
-        tvMensaje.append("en Inicio\n");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.i("MYAPP", "Resumido");
-        Toast toast = Toast.makeText(this,"Resumido",Toast.LENGTH_LONG);
-        toast.show();
-        tvMensaje.append("Resumido\n");
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i("MYAPP", "Pausado");
-        Toast toast = Toast.makeText(this,"Pausado",Toast.LENGTH_LONG);
-        toast.show();
-        tvMensaje.append("Pausado\n");
-
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("MYAPP", "en Detencion");
-        Toast toast = Toast.makeText(this,"en Detencion",Toast.LENGTH_LONG);
-        toast.show();
-        tvMensaje.append("en Detencion\n");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i("MYAPP", "en Reinicio");
-        Toast toast = Toast.makeText(this,"en Reinicio",Toast.LENGTH_LONG);
-        toast.show();
-        tvMensaje.append("en Reinicio\n");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("MYAPP", "Eliminado");
-        Toast toast = Toast.makeText(this,"Eliminado",Toast.LENGTH_LONG);
-        toast.show();
-        tvMensaje.append("Eliminado\n");
+    public void onClick(View view) {
+        Intent intent = new Intent(MainActivity.this,OtraActivity.class);
+        startActivity(intent);
     }
 }
