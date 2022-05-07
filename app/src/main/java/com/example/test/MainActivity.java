ab21d6e5b2a01f3b3cee510c78eb29c0b8192f9a
Omar Sulca <omarsulca@gmail.com>
2022-05-07 16:27:37 -0500

    Intents Implicitos y Explicitos

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,30 +3,37 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
+    EditText etNombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        etNombre = findViewById(R.id.etNombre);
     }
 
     public void AbrirActivity2(View view) {
+        String nombre = etNombre.getText().toString();
         Intent intent = new Intent(MainActivity.this, OtraActivity.class);
+        intent.putExtra("nombre", nombre);
         startActivity(intent);
     }
 
     public void AbrirActivity3(View view) {
-        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:" + "(062)517727"));
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/test/OtraActivity.java
+++ app/src/main/java/com/example/test/OtraActivity.java
@@ -3,6 +3,7 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class OtraActivity extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class OtraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_otra);
+
+        String nombre = getIntent().getExtras().getString("nombre");
+        TextView tvNombre = findViewById(R.id.tvNombre);
+        tvNombre.setText(nombre);
     }
 }
